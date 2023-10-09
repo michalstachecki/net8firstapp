d3e561647865280ca08d00219cd0f35cf8d9f220
michalstachecki <stacheckimichals@gmail.com>
2023-10-09 09:55:58 +0200

    Init project. TwoYearsDigitMax usage example

---
+++ Main/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+CultureInfo clonedInvariantCulture = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+clonedInvariantCulture.DateTimeFormat.Calendar.TwoDigitYearMax = 2039; // Use any desired cutoff value.
+
+DateTime dt = DateTime.Parse("12/25/45", clonedInvariantCulture);
+DateTime dt2 = DateTime.Parse("12/25/35", clonedInvariantCulture);
+Console.WriteLine(dt.ToString());
+Console.WriteLine(dt2.ToString());
